510ef12b413f6513cb583af03965a34bf493776f
An-Fruit <92803192+An-Fruit@users.noreply.github.com>
2022-08-23 18:47:11 -0500

    Add files via upload

--- RomanNumerals.java
+++ RomanNumerals.java
@@ -1,1 +1,47 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import static java.lang.System.*;
+import static java.lang.Integer.*;
+import static java.lang.Double.*;
+import static java.lang.Character.*;
+import static java.util.Collections.*;
+import static java.lang.Math.*;
+import static java.util.Arrays.*;
+
+public class RomanNumerals{
+
+	static int[] vals = new int[] {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+	static String[] roman = "M CM D CD C XC L XL X IX V IV I".split(" ");
+
+    public String convert(int n){
+		String ret = "";
+		for(int i = 0; i < vals.length; i++){
+			while(n >= vals[i]){
+				n -= vals[i];
+				ret += roman[i];
+			}
+		}
+		return ret;
+    }
+
+    public void run() throws Exception{
+
+	    //Scanner f = new Scanner(new File("RomanNumerals.dat"));
+	    Scanner f = new Scanner(System.in);
+
+		while(f.hasNext()){
+			int n = f.nextInt();
+			System.out.println(convert(n));
+		}
+
+        f.close();
+
+	}
+	public static void main(String[] args) throws Exception{
+
+	new RomanNumerals().run();
+
+	}
+}
 
