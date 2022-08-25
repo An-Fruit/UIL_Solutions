5512b738eae84bedb5aeeb2b4d0fa54e90ecb571
An-Fruit <92803192+An-Fruit@users.noreply.github.com>
2022-08-25 08:56:46 -0500

    Add files via upload

--- Mineshaft.java
+++ Mineshaft.java
@@ -3,10 +3,33 @@ import java.io.*;
 
 public class Mineshaft {
 	char[][][] mat;
-	
-	public void go(int z, int x, int y) {
-		if(z > 0 && z < mat.length && x > 0 && x < mat[z].length && y > 0 && y < mat[z][x].length && mat[z][x][y] != '#') {
-			
+	int[][][] shadow;
+	boolean dead, used;
+	int durability, seconds;
+	public void go(int z, int x, int y, int steps) {
+		boolean used = false;
+		int time = 0;
+		if(z > 0 && z < mat.length && x > 0 && x < mat[z].length && y > 0 && y < mat[z][x].length && (mat[z][x][y] != '#' || mat[z][x][y] == '%' && durability == 0) && steps < shadow[z][x][y]) {
+			if(mat[z][x][y] == 'E') {
+				dead = false;
+				seconds = shadow[z][x][y];
+				return;
+			}
+			if(mat[z][x][y] == '%') {
+				used = true;
+				time += 3;
+				durability--;
+			}
+			mat[z][x][y] = '#';
+			shadow[x][y][z] = steps;
+			go(z + 1, x, y, steps + 2 + time);
+			go(z - 1, x, y, steps + 3 + time);
+			go(z, x + 1, y, steps + 1 + time);
+			go(z, x - 1, y, steps + 1 + time);
+			go(z, x, y + 1, steps + 1 + time);
+			go(z, x, y - 1, steps + 1 + time);
+			if(used) durability++;
+			mat[z][x][y] = '.';
 		}
 	}
 	
@@ -14,14 +37,26 @@ public class Mineshaft {
 		Scanner f = new Scanner(System.in);
 		int t = f.nextInt();
 		while(t-- > 0) {
+			dead = true;
 			int floor = f.nextInt();
 			int r = f.nextInt();
 			int c = f.nextInt();
-			int durability = f.nextInt();
+			durability = f.nextInt();
+			seconds = 0;
 			mat = new char[floor][r][c];
 			for(int i = 0; i < mat.length; i++) {
 				for(int j = 0; j < mat[i].length; j++) {
 					mat[i][j] = f.next().toCharArray();
+				}
+			}
+			
+			
+			
+			for(int i = 0; i < shadow.length; i++) {
+				for(int j = 0; j < shadow[i].length; j++) {
+					for(int k = 0; k < shadow[i][j].length; k++) {
+						shadow[i][j][k] = Integer.MAX_VALUE;
+					}
 				}
 			}
 			
@@ -38,9 +73,13 @@ public class Mineshaft {
 					}
 				}
 			}
-			f.close();
+			go(startloc[0], startloc[1], startloc[2], 0);
+			if(!dead) System.out.println(seconds + " SECONDS");
+			else System.out.println("DEAD");
 			
 		}
+		
+		f.close();
 	}
 	public static void main(String[] args) throws Exception{
 		new Mineshaft().run();
